ff6b2ef6e36767caea63be5cede3fbebfcf01044
Marcela <marcelamm6@gmail.com>
2022-06-18 15:12:48 -0300

    the-end

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let comer = document.querySelector(".comida");
+const btn = document.querySelector(".btn");
+
+const comidas = ["Pizza", "Lasanha", "Coxinha", "Hamburguer"];
+
+//  = Math.trunc(Math.random() * comidas.length);
+
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+  let comidasIndex = Math.trunc(Math.random() * comidas.length);
+  console.log(comidasIndex);
+  comer.innerHTML = comidas[comidasIndex];
+});
